ed4235ee277f48086b118b37d4b8163efb995d50
huajianjiang <developer.huajianjiang@gmail.com>
2017-07-17 08:03:56 +0800

    Initial commit

---
+++ magic-annotations/src/main/java/magic/annotation/RespondPermission.java
@@ -0,0 +1,18 @@
+package magic.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * <p>Author: Huajian Jiang
+ * <br>Date: 2017/7/17
+ * <br>Email: developer.huajianjiang@gmail.com
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface RespondPermission {
+}
